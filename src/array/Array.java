6b41f488cac312cb93e4cf66249983e8f3db0a4b
Hiba-S <hiba.sabouh.98@gmail.com>
2020-12-11 23:40:45 +0200

    commit

---
+++ src/array/Array.java
@@ -0,0 +1,18 @@
+
+package array;
+
+public class Array {
+
+    public double arraySum ( double a[]){
+        double res = 0 ;
+        for (int i=0 ; i<a.length ; i++){
+            res+=a[i];
+        }
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
